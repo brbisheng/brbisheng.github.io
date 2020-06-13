20575627618699602fe17367e3bfdc44305fdc67
Sheng BI <brbisheng@gmail.com>
2020-06-14 01:01:03 +0800

    Update trial1.js

--- assets/source/cc/trial1.js
+++ assets/source/cc/trial1.js
@@ -1,3 +1,24 @@
 function myFunction() {
   document.getElementById("demo").style.color = "blue";
 }
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+var path1 = "p1/yangshen/qijue/2.txt";
+function r() {readTextFile(path1);}
